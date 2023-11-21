c0bf2efa543f5c0fd15c93cc172398fc1c82e5d3
Nadezhda Ilieva <nadezhda.ilieva@epfl.ch>
2023-11-21 14:15:59 +0100

    fixed transforms

--- datasets/TransformDataset.py
+++ datasets/TransformDataset.py
@@ -4,16 +4,11 @@ from PIL import Image
 from torch.utils.data import Dataset
 
 class TransformDataset(Dataset):
-    def __init__(self, base_dataset, image_transform=None, gt_transform=None):
+    def __init__(self, base_dataset, random_transform = None, image_transform=None, gt_transform=None):
         self.base_dataset = base_dataset
+        self.random_transform = random_transform
         self.image_transform = image_transform
         self.gt_transform = gt_transform
-
-        # both None or both different than None
-        if image_transform == None and gt_transform != None:
-            raise ValueError("Invalid transforms!")
-        if image_transform != None and gt_transform == None:
-            raise ValueError("Invalid transforms!")
 
     def __len__(self):
         return len(self.base_dataset)
@@ -21,11 +16,18 @@ class TransformDataset(Dataset):
     def __getitem__(self, idx):
         # get image and groundtruth
         image, ground_truth = self.base_dataset[idx]
+        # make a dictionary
+        sample = {"image": image, "ground_truth": ground_truth}
+        # apply random transform to the sample (applies to both image and groundtruth)
+        if self.random_transform:
+            sample = self.random_transform(sample)
+        # get image and groundtruth
+        image, ground_truth = sample["image"], sample["ground_truth"]
         # apply image transform
         if self.image_transform:
             image = self.image_transform(image)
         # apply groundtruth transform
         if self.gt_transform:
             ground_truth = self.gt_transform(ground_truth)
-
+        # return image and groundtruth
         return image, ground_truth
--- train_utils.py
+++ train_utils.py
@@ -4,33 +4,36 @@ import argparse
 from omegaconf import OmegaConf
 import wandb
 import torch
-import torchvision.transforms as transforms
+import torchvision.transforms.v2 as transforms
 from models.UNetV1 import UNetV1
 from models.UNetV2 import UNetV2
 from datasets.BaseDataset import BaseDataset
 from datasets.TransformDataset import TransformDataset
 
 def prepare_transforms(args):
-    transform = []
+    # prepare random transform (to be applied at the same time to both image and groundtruth, for consistency)
+    # the goal is to avoid scenarios like flipping the image but not the groundtruth.
+    random_transform = []
     # random resized crop
     if args.random_resized_crop:
         print(f"Using RandomResizedCrop with output size={tuple(args.output_size)}, scale={tuple(args.random_resized_crop_scale)}.")
-        transform.append(transforms.RandomResizedCrop(size=tuple(args.output_size), scale=tuple(args.random_resized_crop_scale)))
+        random_transform.append(transforms.RandomResizedCrop(size=tuple(args.output_size), scale=tuple(args.random_resized_crop_scale)))
     # random horizontal flip
     if args.random_horizontal_flip:
         print("Using RandomHorizontalFlip.")
-        transform.append(transforms.RandomHorizontalFlip())
+        random_transform.append(transforms.RandomHorizontalFlip())
     # random vertical flip
     if args.random_vertical_flip:
         print("Using RandomVerticalFlip.")
-        transform.append(transforms.RandomVerticalFlip())
+        random_transform.append(transforms.RandomVerticalFlip())
     # random rotation
     if args.random_rotation:
         print(f"Using RandomRotation with degrees={args.degrees}.")
-        transform.append(transforms.RandomRotation(degrees=args.degrees))
-    # define image_transfom and gt_transform
-    image_transform = transform.copy()
-    gt_transform = transform.copy()
+        random_transform.append(transforms.RandomRotation(degrees=args.degrees))
+    # prepare additional deterministic transforms for image and groundtruth (these do not need to be applied at the same time)
+    # the goal is to allow some flexibility in the transforms applied to the image and groundtruth, e.g. color jitter for image only.
+    image_transform = []
+    gt_transform = []
     # color jitter (for image only)
     if args.color_jitter:
         print(f"Using ColorJitter with brightness={args.brightness}, contrast={args.contrast}, saturation={args.saturation}, hue={args.hue}.")
@@ -44,16 +47,20 @@ def prepare_transforms(args):
         print("Using Normalize with mean=(0.5, 0.5, 0.5) and std=(0.5, 0.5, 0.5).")
         image_transform.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)))
         # gt_transform.append(transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))) -> No normalization for groundtruth
-    # compose
+    print("random_transform: ", random_transform)
+    print("image_transform: ", image_transform)
+    print("gt_transform: ", gt_transform)
+    # compose transforms
+    random_transform = transforms.Compose(random_transform)
     image_transform = transforms.Compose(image_transform)
     gt_transform = transforms.Compose(gt_transform)
-    # return (image_transform, gt_transform)
-    return image_transform, gt_transform
+    # return (random_transform, image_transform, gt_transform)
+    return random_transform, image_transform, gt_transform
 
 
 def prepare_data(args):
     # get image and groundtruth transforms (for train set)
-    image_transform, gt_transform = prepare_transforms(args)
+    random_transform, image_transform, gt_transform = prepare_transforms(args)
     # create transforms for images and groundtruths for validation and test sets
     tt_transform_image = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])  # TODO: Should we always normalize?
     tt_transform_gt = transforms.Compose([transforms.ToTensor()])  # No normalization for groundtruth!
@@ -65,9 +72,9 @@ def prepare_data(args):
     train_set, val_set, test_set = torch.utils.data.random_split(
         dataset, [int(args.train_size*len(dataset)), int(args.val_size*len(dataset)), int(args.test_size*len(dataset))])
     # apply transforms
-    train_set = TransformDataset(train_set, image_transform=image_transform, gt_transform=gt_transform)
-    val_set = TransformDataset(val_set, image_transform=tt_transform_image, gt_transform=tt_transform_gt)
-    test_set = TransformDataset(test_set, image_transform=tt_transform_image, gt_transform=tt_transform_gt)
+    train_set = TransformDataset(train_set, random_transform=random_transform, image_transform=image_transform, gt_transform=gt_transform)
+    val_set = TransformDataset(val_set, random_transform=None, image_transform=tt_transform_image, gt_transform=tt_transform_gt)
+    test_set = TransformDataset(test_set, random_transform=None, image_transform=tt_transform_image, gt_transform=tt_transform_gt)
     # create data loaders
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=args.batch_size, shuffle=True)
