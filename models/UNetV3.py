113acb2790e6fe77b42e03c138d905e6fa7453af
Stefanstud <stefan.krsteski@gmail.com>
2023-11-27 12:18:15 +0100

    Add UNetV3 wrapper class

---
+++ models/UNetV3.py
@@ -0,0 +1,22 @@
+""" Wrapper class for brain-segmentation-pytorch (unet) by mateuszbuda. 
+    Original code can be found at: https://github.com/mateuszbuda/brain-segmentation-pytorch"""
+import torch
+import torch.nn as nn
+import segmentation_models_pytorch as smp
+
+
+class UNetV3(nn.Module):
+    def __init__(
+        self, encoder="resnet50", encoder_weights="imagenet", classes=1, activation=None
+    ):
+        super().__init__()
+        # Load the model from Torch Hub
+        self.model = smp.Unet(
+            encoder_name=encoder,
+            encoder_weights=encoder_weights,
+            classes=classes,
+            activation=activation,
+        )
+
+    def forward(self, x):
+        return self.model(x)
